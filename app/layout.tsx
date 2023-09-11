0fe7b70752035e446db7df90f6cd122af52cbcba
Artiqt <gignt0646@mail.ru>
2023-09-11 18:48:39 +0300

    Add authenticationÐ

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import '@/styles/globals.css'
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
-import {ReactNode} from "react";
+import type { Metadata } from 'next';
 
-const inter = Open_Sans({ subsets: ['latin'] })
+import { ClerkProvider } from '@clerk/nextjs';
+import { Open_Sans } from 'next/font/google';
+import { ReactNode } from 'react';
+import '@/styles/globals.css';
+
+const inter = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Discord | Your place for communication'
-}
+  title: 'Discord | Your place for communication',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
